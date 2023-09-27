b0ef7fdb039417ab70506c49534b0bc9e299be21
Ekaterina Saukova <esaukova@outlook.com>
2023-09-27 11:06:00 +0300

    Updated from Selenium to Selenide

--- src/test/java/ru/netology/testmode/data/DataGenerator.java
+++ src/test/java/ru/netology/testmode/data/DataGenerator.java
@@ -7,10 +7,7 @@ import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import lombok.Value;
-
-
 import java.util.Locale;
-
 import static io.restassured.RestAssured.given;
 
 public class DataGenerator {
@@ -27,16 +24,11 @@ public class DataGenerator {
     }
 
     private static void sendRequest(RegistrationDto user) {
-        // TODO: отправить запрос на указанный в требованиях path, передав в body запроса объект user
-        //  и не забудьте передать подготовленную спецификацию requestSpec.
-        //  Пример реализации метода показан в условии к задаче.
-
         // Given - When - Then
-        //
         given()
                 .log().all()
                 .spec(requestSpec)
-                .body(new Gson().toJson(user))
+                .body(user)
                 // Action
                 .when()
                 .post("/api/system/users")
@@ -49,15 +41,11 @@ public class DataGenerator {
 
 
     public static String getRandomLogin() {
-        // TODO: добавить логику для объявления переменной login и задания её значения, для генерации
-        //  случайного логина используйте faker
         String login = faker.name().username();
         return login;
     }
 
     public static String getRandomPassword() {
-        // TODO: добавить логику для объявления переменной password и задания её значения, для генерации
-        //  случайного пароля используйте faker
         String password = faker.internet().password();
         return password;
     }
@@ -67,14 +55,11 @@ public class DataGenerator {
         }
 
         public static RegistrationDto getUser(String status) {
-            // TODO: создать пользователя user используя методы getRandomLogin(), getRandomPassword() и параметр status
             RegistrationDto user = new RegistrationDto(getRandomLogin(), getRandomPassword(), status);
             return user;
         }
 
         public static RegistrationDto getRegisteredUser(String status) {
-            // TODO: объявить переменную registeredUser и присвоить ей значение возвращённое getUser(status).
-            // Послать запрос на регистрацию пользователя с помощью вызова sendRequest(registeredUser)
             var registeredUser = getUser(status);
             sendRequest(registeredUser);
             return registeredUser;
--- src/test/java/ru/netology/testmode/test/AuthTest.java
+++ src/test/java/ru/netology/testmode/test/AuthTest.java
@@ -1,59 +1,34 @@
 package ru.netology.testmode.test;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.*;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.open;
 import static ru.netology.testmode.data.DataGenerator.Registration.getRegisteredUser;
 import static ru.netology.testmode.data.DataGenerator.Registration.getUser;
 import static ru.netology.testmode.data.DataGenerator.getRandomLogin;
 import static ru.netology.testmode.data.DataGenerator.getRandomPassword;
 
 class AuthTest {
-    private WebDriver driver;
-
-    @BeforeAll
-    static void setUpAll() {
-        WebDriverManager.chromedriver().setup();
-    }
 
     @BeforeEach
     void setup() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage"); //overcome limited resource problems
-        options.addArguments("--no-sandbox");  //Bypass OS security model
-        options.addArguments("--headless=new");
-        driver = new ChromeDriver(options);
-        driver.get("http://localhost:9999");
+        open("http://localhost:9999");
     }
-
-    @AfterEach
-    void tearDown() {  //после выполнения тестов
-        driver.quit();
-        driver = null;
-    }
-
-
     @Test
     @DisplayName("Should successfully login with active registered user")
     void shouldSuccessfulLoginIfRegisteredActiveUser() {
         var registeredUser = getRegisteredUser("active");
-        // TODO: добавить логику теста, в рамках которого будет выполнена попытка входа в личный кабинет с учётными
-        //  данными зарегистрированного активного пользователя, для заполнения полей формы используйте
-        //  пользователя registeredUser
-        driver.findElement(By.cssSelector("[data-test-id='login'] input")).sendKeys(registeredUser.getLogin());
-        driver.findElement(By.cssSelector("[data-test-id='password'] input")).sendKeys(registeredUser.getPassword());
-        driver.findElement(By.cssSelector("[data-test-id='action-login']")).click();
-        String text = driver.findElement(By.cssSelector("[id='root'] h2")).getText();
-        assertEquals("Личный кабинет", text.trim());
+        $("[data-test-id='login'] input").setValue(registeredUser.getLogin());
+        $("[data-test-id='password'] input").setValue(registeredUser.getPassword());
+        $("[data-test-id='action-login']").click();
+        $("[id='root'] h2").shouldHave(exactText("Личный кабинет"));
 
     }
 
@@ -61,13 +36,12 @@ class AuthTest {
     @DisplayName("Should get error message if login with not registered user")
     void shouldGetErrorIfNotRegisteredUser() {
         var notRegisteredUser = getUser("active");
-        // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет
-        //  незарегистрированного пользователя, для заполнения полей формы используйте пользователя notRegisteredUser
-        driver.findElement(By.cssSelector("[data-test-id='login'] input")).sendKeys(notRegisteredUser.getLogin());
-        driver.findElement(By.cssSelector("[data-test-id='password'] input")).sendKeys(notRegisteredUser.getPassword());
-        driver.findElement(By.cssSelector("[data-test-id='action-login']")).click();
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
-        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector("[data-test-id='error-notification']"))));
+        $("[data-test-id='login'] input").setValue(notRegisteredUser.getLogin());
+        $("[data-test-id='password'] input").setValue(notRegisteredUser.getPassword());
+        $("[data-test-id='action-login']").click();
+        $("[data-test-id='error-notification'] .notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldBe(Condition.exactText("Ошибка! Неверно указан логин или пароль"));
 
     }
 
@@ -75,13 +49,13 @@ class AuthTest {
     @DisplayName("Should get error message if login with blocked registered user")
     void shouldGetErrorIfBlockedUser() {
         var blockedUser = getRegisteredUser("blocked");
-        // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет,
-        //  заблокированного пользователя, для заполнения полей формы используйте пользователя blockedUser
-        driver.findElement(By.cssSelector("[data-test-id='login'] input")).sendKeys(blockedUser.getLogin());
-        driver.findElement(By.cssSelector("[data-test-id='password'] input")).sendKeys(blockedUser.getPassword());
-        driver.findElement(By.cssSelector("[data-test-id='action-login']")).click();
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
-        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector("[data-test-id='error-notification']"))));
+        $("[data-test-id='login'] input").setValue(blockedUser.getLogin());
+        $("[data-test-id='password'] input").setValue(blockedUser.getPassword());
+        $("[data-test-id='action-login']").click();
+        $("[data-test-id='error-notification'] .notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldBe(Condition.exactText("Ошибка! Пользователь заблокирован"));
+
     }
 
     @Test
@@ -89,14 +63,12 @@ class AuthTest {
     void shouldGetErrorIfWrongLogin() {
         var registeredUser = getRegisteredUser("active");
         var wrongLogin = getRandomLogin();
-        // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет с неверным
-        //  логином, для заполнения поля формы "Логин" используйте переменную wrongLogin,
-        //  "Пароль" - пользователя registeredUser
-        driver.findElement(By.cssSelector("[data-test-id='login'] input")).sendKeys(wrongLogin);
-        driver.findElement(By.cssSelector("[data-test-id='password'] input")).sendKeys(registeredUser.getPassword());
-        driver.findElement(By.cssSelector("[data-test-id='action-login']")).click();
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
-        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector("[data-test-id='error-notification']"))));
+        $("[data-test-id='login'] input").setValue(wrongLogin);
+        $("[data-test-id='password'] input").setValue(registeredUser.getPassword());
+        $("[data-test-id='action-login']").click();
+        $("[data-test-id='error-notification'] .notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldBe(Condition.exactText("Ошибка! Неверно указан логин или пароль"));
 
     }
 
@@ -105,13 +77,11 @@ class AuthTest {
     void shouldGetErrorIfWrongPassword() {
         var registeredUser = getRegisteredUser("active");
         var wrongPassword = getRandomPassword();
-        // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет с неверным
-        //  паролем, для заполнения поля формы "Логин" используйте пользователя registeredUser,
-        //  "Пароль" - переменную wrongPassword
-        driver.findElement(By.cssSelector("[data-test-id='login'] input")).sendKeys(registeredUser.getLogin());
-        driver.findElement(By.cssSelector("[data-test-id='password'] input")).sendKeys(wrongPassword);
-        driver.findElement(By.cssSelector("[data-test-id='action-login']")).click();
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
-        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector("[data-test-id='error-notification']"))));
+        $("[data-test-id='login'] input").setValue(registeredUser.getLogin());
+        $("[data-test-id='password'] input").setValue(wrongPassword);
+        $("[data-test-id='action-login']").click();
+        $("[data-test-id='error-notification'] .notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldBe(Condition.exactText("Ошибка! Неверно указан логин или пароль"));
     }
 }
